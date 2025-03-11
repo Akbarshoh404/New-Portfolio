14bf9985d646137cceda991f3f7ea00705d8cbd0
Akbarshoh <ismatov2009akb@gmail.com>
2025-03-11 05:57:26 +0500

    demo home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,10 +2,36 @@ import React from "react";
 
 import Navabar from "../../Layoutes/Navbar";
 
+import styles from "./style.module.scss";
+
 const Home = () => {
   return (
     <>
       <Navabar />
+
+      <div className={styles.home}>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <div className={styles.emojiContainer}>
+              <p>👨‍💻</p>
+            </div>
+          </div>
+
+          <div className={styles.right}>
+            <p className={styles.p1}>Frontend Developer</p>
+
+            <p className={styles.p2}>Mr.Akbarshokh</p>
+
+            <p className={styles.p3}>
+              Potřebujete vytvořit webové stránky ? Při tvorbě webových stránek
+              zajišťuji celkový design od návrhu až po realizaci. Perfektní
+              technickou stránku zajistišťuji též, aby všechno běželo přesně jak
+              má. Kdyby by Vás mé portofilo přípradně zkušenosti zaujaly -
+              napište mi !
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
