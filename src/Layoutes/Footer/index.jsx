6c3fb1290b79a26b1893ea5871d95ff1163299fe
Akbarshoh <ismatov2009akb@gmail.com>
2025-03-18 23:29:44 +0500

    responsive footer

--- src/Layoutes/Footer/index.jsx
+++ src/Layoutes/Footer/index.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-
 import styles from "./style.module.scss";
 
 const Footer = () => {
+  const isMobile = window.innerWidth <= 768;
+
   return (
-    <>
-      <div className={styles.footer}>
-        <div className={styles.container}>
+    <div className={styles.footer}>
+      <div className={styles.container}>
+        {!isMobile && (
           <div className={styles.left}>
             <p>Telegram</p>
             <p>Instagram</p>
             <p>Github</p>
           </div>
-
-          <div className={styles.right}>© 2025 Ismatov Akbarshokh</div>
-        </div>
+        )}
+        <div className={styles.right}>© 2025 Ismatov Akbarshokh</div>
       </div>
-    </>
+    </div>
   );
 };
 
